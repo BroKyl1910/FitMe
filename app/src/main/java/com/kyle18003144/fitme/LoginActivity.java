7d951088278f8c3c991371b41e9975b1ebeb00f9
Kyle <brooks.kyle621@gmail.com>
2019-10-02 20:19:06 +0200

    Working on styles and login GUI

---
+++ app/src/main/java/com/kyle18003144/fitme/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.kyle18003144.fitme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
